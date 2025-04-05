8bd02a31496fab595200d13cf7efbe590937f655
AngelEduardoSanchezRamos <angelesr2631@gmail.com>
2025-04-04 22:57:41 -0600

    Add files via upload

---
+++ Practica 10 - Ciclos Visual/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Practica_10._1___Ciclos_Visual
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            listBox1 = new ListBox();
+            listBox2 = new ListBox();
+            listBox3 = new ListBox();
+            listBox4 = new ListBox();
+            label8 = new Label();
+            label9 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(351, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(132, 38);
+            label1.TabIndex = 0;
+            label1.Text = "z = x + x";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(421, 37);
+            label2.Name = "label2";
+            label2.Size = new Size(18, 20);
+            label2.TabIndex = 1;
+            label2.Text = "2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(476, 37);
+            label3.Name = "label3";
+            label3.Size = new Size(18, 20);
+            label3.TabIndex = 2;
+            label3.Text = "3";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 13.2000008F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(214, 106);
+            label4.Name = "label4";
+            label4.Size = new Size(27, 31);
+            label4.TabIndex = 3;
+            label4.Text = "x";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 13.2000008F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(347, 106);
+            label5.Name = "label5";
+            label5.Size = new Size(27, 31);
+            label5.TabIndex = 4;
+            label5.Text = "x";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 13.2000008F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(474, 106);
+            label6.Name = "label6";
+            label6.Size = new Size(27, 31);
+            label6.TabIndex = 5;
+            label6.Text = "x";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 13.2000008F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(596, 106);
+            label7.Name = "label7";
+            label7.Size = new Size(25, 31);
+            label7.TabIndex = 6;
+            label7.Text = "z";
+            // 
+            // listBox1
+            // 
+            listBox1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(178, 152);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(90, 164);
+            listBox1.TabIndex = 7;
+            // 
+            // listBox2
+            // 
+            listBox2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            listBox2.FormattingEnabled = true;
+            listBox2.Location = new Point(313, 152);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(90, 164);
+            listBox2.TabIndex = 8;
+            // 
+            // listBox3
+            // 
+            listBox3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            listBox3.FormattingEnabled = true;
+            listBox3.Location = new Point(440, 152);
+            listBox3.Name = "listBox3";
+            listBox3.Size = new Size(90, 164);
+            listBox3.TabIndex = 9;
+            // 
+            // listBox4
+            // 
+            listBox4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            listBox4.FormattingEnabled = true;
+            listBox4.Location = new Point(561, 152);
+            listBox4.Name = "listBox4";
+            listBox4.Size = new Size(90, 164);
+            listBox4.TabIndex = 10;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.Location = new Point(367, 106);
+            label8.Name = "label8";
+            label8.Size = new Size(18, 20);
+            label8.TabIndex = 11;
+            label8.Text = "2";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.Location = new Point(494, 106);
+            label9.Name = "label9";
+            label9.Size = new Size(18, 20);
+            label9.TabIndex = 12;
+            label9.Text = "3";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.LightSteelBlue;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(314, 352);
+            button1.Name = "button1";
+            button1.Size = new Size(213, 40);
+            button1.TabIndex = 13;
+            button1.Text = "Calcular Valores";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightGray;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(listBox4);
+            Controls.Add(listBox3);
+            Controls.Add(listBox2);
+            Controls.Add(listBox1);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private ListBox listBox3;
+        private ListBox listBox4;
+        private Label label8;
+        private Label label9;
+        private Button button1;
+    }
+}
