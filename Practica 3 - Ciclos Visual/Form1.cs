91e3831f060a14a4aae5d6095c791bf24046fb6a
AngelEduardoSanchezRamos <angelesr2631@gmail.com>
2025-04-04 22:54:21 -0600

    Add files via upload

---
+++ Practica 3 - Ciclos Visual/Form1.cs
@@ -0,0 +1,27 @@
+using System.Numerics;
+
+namespace Practica_3___Ciclos_Visual
+{
+    public partial class Form1 : Form
+    {
+        float f_num, Total = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = Total.ToString();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+            f_num = float.Parse(textBox1.Text);
+            Total += f_num;
+            textBox1.Text = "";
+            textBox1.Focus();
+        }
+    }
+}
